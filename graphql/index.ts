1a372e269d7334b7a4be3527725cca764e824f7f
Kanish Chheda <kanishchheda@gmail.com>
2023-07-15 18:29:31 +0530

    thirteenth attempt

--- graphql/index.ts
+++ graphql/index.ts
@@ -56,32 +56,39 @@ export const createUserMutation = `
 
 export const projectsQuery = `
   query getProjects($category: String, $endCursor: String) {
-    projectSearch(first: 8, after: $endCursor, filter: {category: {eq: $category}}) {
-      pageInfo {
-        hasNextPage
-        hasPreviousPage
-        startCursor
-        endCursor
-      }
-      edges {
-        node {
-          title
-          githubUrl
-          description
-          liveSiteUrl
+  projectSearch(first: 8, after: $endCursor, filter: {
+    category: {
+      or: [
+        { eq: $category },
+        { eq: "all" }
+      ]
+    }
+  }) {
+    pageInfo {
+      hasNextPage
+      hasPreviousPage
+      startCursor
+      endCursor
+    }
+    edges {
+      node {
+        title
+        githubUrl
+        description
+        liveSiteUrl
+        id
+        image
+        category
+        createdBy {
           id
-          image
-          category
-          createdBy {
-            id
-            email
-            name
-            avatarUrl
-          }
+          email
+          name
+          avatarUrl
         }
       }
     }
   }
+}
 `;
 
 export const getProjectByIdQuery = `
