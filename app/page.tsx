137478e1b4a1cfdfaf3195efdde78609b2cac42e
Kanish Chheda <kanishchheda@gmail.com>
2023-07-16 17:45:19 +0530

    2nd attempt

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export const revalidate = 0;
 
 const Home = async ({ searchParams: { category, endCursor } }: Props) => {
   const data = (await fetchAllProjects(
-    (category = null),
+    (category = "*"),
     endCursor
   )) as ProjectSearch;
   const projectsToDisplay = data?.projectSearch?.edges || [];
